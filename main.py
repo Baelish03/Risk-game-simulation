be2423a941c62884ab851a48be06b19d64c2ec71
Emanuele Urso <ursomanuele@gmail.com>
2026-03-02 23:00:21 +0100

    create map and plot map on main

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import world_map
+import time
+
 if "__main__" == __name__:
-    pass
+    start = time.perf_counter()
+    world = world_map.WorldMap()
+    print(f"Time passed: {time.perf_counter() - start:.4f}s")
+    world.plot()
